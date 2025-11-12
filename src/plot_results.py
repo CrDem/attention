f02e84b8a27db6b1725199cebe0c749ce7ed46ec
CrDem <anvakuznetsov@edu.hse.ru>
2025-11-12 16:47:47 +0300

    flash attention with fp16 measurements
    - Extended benchmark to include Flash Attention.
    - Updated all measurements ( FP32 --> FP16 )

--- src/plot_results.py
+++ src/plot_results.py
@@ -7,17 +7,19 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("src/benchmarks/attention_benchmark.csv")
 
 plt.figure(figsize=(8,5))
-plt.plot(df["seq_len"], df["core_ms"], marker="o", label="Attention core")
+plt.plot(df["seq_len"], df["core_ms"], marker="o", alpha = 0.7, label="Attention core")
 plt.plot(df["seq_len"], df["full_ms"], marker="o", label="Full block")
+plt.plot(df["seq_len"], df["flash_ms"], marker="x", ls = '--', alpha = 0.7, label="Flash attention")
 
 plt.xticks(df["seq_len"])
 seq_ticks = sorted(df["seq_len"].unique().tolist())
 plt.xticks(seq_ticks, [str(x) for x in seq_ticks])
 plt.xscale("log", base=2)
+plt.yscale("log")
 
 plt.xlabel("Sequence length")
 plt.ylabel("Time (ms)")
-plt.title("Multi-Head Attention Performance (RTX 2070S)")
+plt.title("Classic Multi-Head vs Flash Attention (RTX 2070S)")
 plt.grid(True, which="both", linestyle="--", linewidth=0.5)
 plt.legend()
 plt.tight_layout()
--- src/test_perf.py
+++ src/test_perf.py
@@ -1,13 +1,15 @@
 import torch
 from attention import MultiHeadAttentionBlock
+from flash_attn.flash_attn_interface import flash_attn_func
 
 batch_size = 1
 d_model = 512
 num_heads = 16
+head_dim = d_model // num_heads
 seq_lens = [64, 128, 256, 512, 1024, 2048, 4096]
 mask = None
 
-attentionBlock = MultiHeadAttentionBlock(d_model, num_heads, dropout=0.0).cuda()
+attentionBlock = MultiHeadAttentionBlock(d_model, num_heads, dropout=0.0).cuda().half()
 
 results = []
 num_iters = 100
@@ -15,14 +17,24 @@ num_iters = 100
 for seq_len in seq_lens:
 
     # Create random input tensors
-    query = torch.randn(batch_size, seq_len, d_model, device='cuda')
-    key = torch.randn(batch_size, seq_len, d_model, device='cuda')
-    value = torch.randn(batch_size, seq_len, d_model, device='cuda')
+    query = torch.randn(batch_size, seq_len, d_model, device='cuda', dtype=torch.float16)
+    key = torch.randn(batch_size, seq_len, d_model, device='cuda', dtype=torch.float16)
+    value = torch.randn(batch_size, seq_len, d_model, device='cuda', dtype=torch.float16)
+
+    # Create qkv tensor for flash_attn_func
+    q = query.view(batch_size * seq_len, num_heads, head_dim)
+    k = key.view(batch_size * seq_len, num_heads, head_dim)
+    v = value.view(batch_size * seq_len, num_heads, head_dim)
+    qkv = torch.stack([q, k, v], dim=1)  # (total_q, 3, num_heads, head_dim)
+    cu_seqlens = torch.arange(0, (batch_size + 1) * seq_len, step=seq_len,
+                          dtype=torch.int32, device='cuda')
 
     startCore = torch.cuda.Event(enable_timing=True)
     endCore = torch.cuda.Event(enable_timing=True)
     startFull = torch.cuda.Event(enable_timing=True)
     endFull = torch.cuda.Event(enable_timing=True)
+    startFlash = torch.cuda.Event(enable_timing=True)
+    endFlash = torch.cuda.Event(enable_timing=True)
 
     # GPU warm up
     for _ in range(5):
@@ -44,19 +56,28 @@ for seq_len in seq_lens:
     endFull.record()
     torch.cuda.synchronize()
 
+    # built-in flash attention measure
+    startFlash.record()
+    for _ in range(num_iters):
+        flash_attn_func(qkv, cu_seqlens, 0.0, seq_len, softmax_scale=None, causal=False)
+    endFlash.record()
+    torch.cuda.synchronize()
+
     coreAvgTimeMs = startCore.elapsed_time(endCore) / num_iters
     fullAvgTimeMs = startFull.elapsed_time(endFull) / num_iters
+    flashAvgTimeMs = startFlash.elapsed_time(endFlash) / num_iters
     print(f"seq len: {seq_len} tokens")
     print(f"attention core: {coreAvgTimeMs:.3f} ms")
-    print(f"full block: {fullAvgTimeMs:.3f} ms\n")
+    print(f"full block: {fullAvgTimeMs:.3f} ms")
+    print(f"flash attention: {flashAvgTimeMs:.3f} ms\n")
 
-    results.append((seq_len, coreAvgTimeMs, fullAvgTimeMs))
+    results.append((seq_len, coreAvgTimeMs, fullAvgTimeMs, flashAvgTimeMs))
 
 #saving results
 import csv
 
 with open("src/benchmarks/attention_benchmark.csv", "w", newline="") as f:
     writer = csv.writer(f)
-    writer.writerow(["seq_len", "core_ms", "full_ms"])
+    writer.writerow(["seq_len", "core_ms", "full_ms", "flash_ms"])
     writer.writerows(results)
 print("results saved to src/benchmarks/attention_benchmark.csv")
